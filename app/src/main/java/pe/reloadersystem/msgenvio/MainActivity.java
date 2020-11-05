29a66d917a0e794bc22b097f5d536b53f416f9b8
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-11-05 13:06:16 -0500

    varios sms tiempo delay

--- app/src/main/java/pe/reloadersystem/msgenvio/MainActivity.java
+++ app/src/main/java/pe/reloadersystem/msgenvio/MainActivity.java
@@ -8,42 +8,66 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Telephony;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import pe.reloadersystem.msgenvio.Servicios.Retrofit.ItemPostsms;
-import pe.reloadersystem.msgenvio.Servicios.WebServiceListarSms;
-import pe.reloadersystem.msgenvio.Servicios.WebServiceUpdateSms;
+import okhttp3.ResponseBody;
+import pe.reloadersystem.msgenvio.Servicios.Retrofit.HelperWs;
+import pe.reloadersystem.msgenvio.Servicios.Retrofit.MethodWs;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
     Button btnEnviar;
+    String sms_destinatario;
+    String sms_mensaje;
 
-    String SENT = "SMS_SENT";
-    String DELIVERED = "SMS_DELVERED";
-    PendingIntent sendPI, deliveredPI;
-    BroadcastReceiver smsSendReceiver, smsDeliveredReceiver;
+    private static final String SMS_SENT_ACTION = "com.mycompany.myapp.SMS_SENT";
+    private static final String SMS_DELIVERED_ACTION = "com.mycompany.myapp.SMS_DELIVERED";
+    private int requestCode = 33;
+
+    private static final String EXTRA_NUMBER = "number";
+    private static final String EXTRA_MESSAGE = "message";
+    private static final String EXTRA_ID = "sms_id";
+
+    private BroadcastReceiver resultsReceiver;
+    private IntentFilter intentFilter;
+
+    int count = 0;
+    String cadena_respuesta;
+
+    Handler handler = new Handler();
+
+    private final int TIEMPO = 10000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnEnviar = findViewById(R.id.btnEnviarMSG);
-
-        sendPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
-        deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
-
-        WebServiceListarSms.listarSms(this);
-
+        resultsReceiver = new SmsResultReceiver();
+        intentFilter = new IntentFilter(SMS_SENT_ACTION);
+        intentFilter.addAction(SMS_DELIVERED_ACTION);
 
         btnEnviar.setOnClickListener(this);
     }
@@ -51,32 +75,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-//        String numero = "961162784";
-        String numero = "997248787";
-        String mensaje = "Reloader System SMS Enviado en apagado";
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
                     MY_PERMISSIONS_REQUEST_SEND_SMS);
         } else {
-            SmsManager sms = SmsManager.getDefault();
+
+            MethodWs methodWs = HelperWs.getConfiguration(this).create(MethodWs.class);
+            Call<ResponseBody> responseBodyCall = methodWs.getDatosSMS();
+            responseBodyCall.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                    if (response.isSuccessful()) {
+                        ResponseBody informacion = response.body();
+                        try {
+                            cadena_respuesta = informacion.string();
+
+                            SendMessage(cadena_respuesta);
 
 
+                        } catch (Exception e) {
+                            Log.e("LogResponseError", e.toString());
+                        }
+                    }
+                }
 
-            ArrayList<String> parts  = sms.divideMessage("El dispositivo móvil basado en GSM se encarga de la segmentación en la que se rompen los mensajes a varias partes para enviar en ejecución y también el ensamblaje de los mensajes de varias partes en un mensaje en el recibo.");
-//            sms.sendTextMessage(numero, null, parts, sendPI, deliveredPI);
-            int numParts = parts.size();
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
 
-            ArrayList<PendingIntent> sentIntents = new ArrayList<PendingIntent>();
-            ArrayList<PendingIntent> deliveryIntents = new ArrayList<PendingIntent>();
+                }
+            });
+        }
+    }
 
-            for (int i = 0; i < numParts; i++) {
-                sentIntents.add(sendPI);
-                deliveryIntents.add(deliveredPI);
+    private void SendMessage(String cadena_respuesta) {
+
+        try {
+            JSONObject respuesta = new JSONObject(cadena_respuesta);
+            JSONArray data = respuesta.getJSONArray("data");
+
+            String datostosend = String.valueOf(data.length());
+            Log.d("datostosend", datostosend);
+
+            if (count < data.length()) {
+                int code = (int) ((JSONObject) data.get(count)).get("sms_id");
+                sms_destinatario = ((JSONObject) data.get(count)).getString("sms_destinatario");
+                sms_mensaje = ((JSONObject) data.get(count)).getString("sms_mensaje");
+
+                SmsManager sms = SmsManager.getDefault();
+
+                Intent sentIntent = new Intent(SMS_SENT_ACTION);
+                Intent deliveredIntent = new Intent(SMS_DELIVERED_ACTION);
+
+                sentIntent.putExtra(EXTRA_NUMBER, sms_destinatario);
+                sentIntent.putExtra(EXTRA_MESSAGE, sms_mensaje);
+                sentIntent.putExtra(EXTRA_ID, code);
+
+                deliveredIntent.putExtra(EXTRA_NUMBER, sms_destinatario);
+                deliveredIntent.putExtra(EXTRA_MESSAGE, sms_mensaje);
+                deliveredIntent.putExtra(EXTRA_ID, code);
+
+                ArrayList<String> parts = sms.divideMessage(sms_mensaje);
+                int numParts = parts.size();
+
+                ArrayList<PendingIntent> sentIntents = new ArrayList<>();
+                ArrayList<PendingIntent> deliveryIntents = new ArrayList<>();
+
+                PendingIntent sentPI = PendingIntent.getBroadcast(this,
+                        requestCode,
+                        sentIntent,
+                        PendingIntent.FLAG_ONE_SHOT);
+
+                PendingIntent deliveredPI = PendingIntent.getBroadcast(this,
+                        requestCode,
+                        deliveredIntent,
+                        PendingIntent.FLAG_ONE_SHOT);
+
+                for (int i = 0; i < numParts; i++) {
+                    sentIntents.add(sentPI);
+                    deliveryIntents.add(deliveredPI);
+                    sms.sendMultipartTextMessage(sms_destinatario, null, parts, sentIntents, deliveryIntents);
+                }
+
             }
-
-            sms.sendMultipartTextMessage(numero, null, parts , sentIntents, deliveryIntents);
+        } catch (Exception e) {
+            Log.e("LogErrorJson", e.toString());
         }
     }
 
@@ -84,68 +168,127 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onPause() {
         super.onPause();
 
-        unregisterReceiver(smsDeliveredReceiver);
-        unregisterReceiver(smsSendReceiver);
+        if (resultsReceiver != null) {
+            unregisterReceiver(resultsReceiver);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        smsSendReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-
-                switch (getResultCode()) {
-                    case Activity
-                            .RESULT_OK:
-                        // Toast.makeText(context, "SMS send", Toast.LENGTH_SHORT).show();
-
-                        ItemPostsms loguinRequest = new ItemPostsms(214, "sms_send");
-
-                        WebServiceUpdateSms.updateSms(context, loguinRequest);
-
-
-                        break;
-
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(context, "Generic failure!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(context, "sin Servicio", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(context, "Null PDU", Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(context, "Radio Off!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                }
-            }
-        };
-
-        smsDeliveredReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(context, "SMS delivered!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(context, "SMS not delivered!", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        };
-
-        registerReceiver(smsSendReceiver, new IntentFilter(SENT));
-        registerReceiver(smsDeliveredReceiver, new IntentFilter(DELIVERED));
+        if (resultsReceiver != null) {
+            registerReceiver(resultsReceiver, intentFilter);
+        }
     }
 
 
+    private class SmsResultReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String result = null;
+            String action = intent.getAction();
+
+            String number = intent.getStringExtra(EXTRA_NUMBER);
+            String message = intent.getStringExtra(EXTRA_MESSAGE);
+            String sms_id = intent.getStringExtra(EXTRA_ID);
+
+            if (SMS_SENT_ACTION.equals(action)) {
+                int resultCode = getResultCode();
+                try {
+                    JSONObject body = new JSONObject();
+                    body.put(EXTRA_NUMBER, number);
+                    body.put(EXTRA_MESSAGE, message);
+                    body.put(EXTRA_ID, sms_id);
+                    body.put("resultCode", resultCode);
+                    result = translateSentResult(body);
+                    Toast.makeText(context, number + " - " + message + result, Toast.LENGTH_SHORT).show();
+
+                    ejecutarTarea();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    Toast.makeText(context, "Broadcast de envio", Toast.LENGTH_SHORT).show();
+                }
+            } else if (SMS_DELIVERED_ACTION.equals(action)) {
+                SmsMessage sms = null;
+                byte[] pdu = intent.getByteArrayExtra("pdu");
+                String format = intent.getStringExtra("format");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && format != null) {
+//                        sms = SmsMessage.createFromPdu(pdu, format);
+                    sms = SmsMessage.createFromPdu(pdu);
+                } else {
+                    sms = SmsMessage.createFromPdu(pdu);
+                }
+
+                result = "Delivery result : " + translateDeliveryStatus(sms.getStatus());
+
+                Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private String translateSentResult(JSONObject params) {
+        String mensaje = null;
+        System.out.println(params);
+        try {
+            switch (params.getInt("resultCode")) {
+                case Activity.RESULT_OK:
+                    mensaje = "Activity.RESULT_OK";
+                    break;
+                case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                    mensaje = "SmsManager.RESULT_ERROR_GENERIC_FAILURE";
+                    break;
+                case SmsManager.RESULT_ERROR_RADIO_OFF:
+                    mensaje = "SmsManager.RESULT_ERROR_RADIO_OFF";
+                    break;
+                case SmsManager.RESULT_ERROR_NULL_PDU:
+                    mensaje = "SmsManager.RESULT_ERROR_NULL_PDU";
+                    break;
+                case SmsManager.RESULT_ERROR_NO_SERVICE:
+                    mensaje = "SmsManager.RESULT_ERROR_NO_SERVICE";
+                    break;
+                default:
+                    mensaje = "Unknown error code";
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Log.e("SINCODIGO", ex.getMessage());
+        }
+
+        return mensaje;
+    }
+
+    String translateDeliveryStatus(int status) {
+        switch (status) {
+            case Telephony.Sms.STATUS_COMPLETE:
+                return "Sms.STATUS_COMPLETE";
+            case Telephony.Sms.STATUS_FAILED:
+                return "Sms.STATUS_FAILED";
+            case Telephony.Sms.STATUS_PENDING:
+                return "Sms.STATUS_PENDING";
+            case Telephony.Sms.STATUS_NONE:
+                return "Sms.STATUS_NONE";
+            default:
+                return "Unknown status code";
+        }
+    }
+
+
+    public void ejecutarTarea() {
+        handler.postDelayed(new Runnable() {
+            public void run() {
+
+                count = count + 1;
+                SendMessage(cadena_respuesta);
+
+                handler.postDelayed(this, TIEMPO);
+            }
+
+        }, TIEMPO);
+
+    }
 }
--- app/src/main/java/pe/reloadersystem/msgenvio/Servicios/WebServiceListarSms.java
+++ app/src/main/java/pe/reloadersystem/msgenvio/Servicios/WebServiceListarSms.java
@@ -1,7 +1,14 @@
 package pe.reloadersystem.msgenvio.Servicios;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.telephony.SmsManager;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import okhttp3.ResponseBody;
 import pe.reloadersystem.msgenvio.Servicios.Retrofit.HelperWs;
@@ -12,9 +19,16 @@ import retrofit2.Response;
 
 public class WebServiceListarSms {
 
+
     public static void listarSms(Context context) {
+
+        PendingIntent sendPI = null;
+        PendingIntent deliveredPI = null;
+
         MethodWs methodWs = HelperWs.getConfiguration(context).create(MethodWs.class);
         Call<ResponseBody> responseBodyCall = methodWs.getDatosSMS();
+        final PendingIntent finalSendPI = sendPI;
+        final PendingIntent finalDeliveredPI = deliveredPI;
         responseBodyCall.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
@@ -23,6 +37,30 @@ public class WebServiceListarSms {
                     ResponseBody informacion = response.body();
                     try {
                         String cadena_respuesta = informacion.string();
+                        JSONObject respuesta = new JSONObject(cadena_respuesta);
+                        JSONArray data = respuesta.getJSONArray("data");
+                        for (int a = 0; a < data.length(); a++) {
+                            JSONObject obj = (JSONObject) data.get(a);
+                            int code = (int) obj.get("sms_id");
+                            String sms_destinatario = obj.getString("sms_destinatario");
+                            String sms_mensaje = obj.getString("sms_mensaje");
+
+                            SmsManager sms = SmsManager.getDefault();
+
+                            ArrayList<String> parts = sms.divideMessage(sms_mensaje);
+                            int numParts = parts.size();
+
+                            ArrayList<PendingIntent> sentIntents = new ArrayList<PendingIntent>();
+                            ArrayList<PendingIntent> deliveryIntents = new ArrayList<PendingIntent>();
+
+                            for (int i = 0; i < numParts; i++) {
+                                sentIntents.add(finalSendPI);
+                                deliveryIntents.add(finalDeliveredPI);
+                            }
+                            sms.sendMultipartTextMessage(sms_destinatario, null, parts, sentIntents, deliveryIntents);
+                        }
+
+
                         Log.e("LogResponse", cadena_respuesta);
 
                     } catch (Exception e) {
